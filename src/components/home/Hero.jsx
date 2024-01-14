828fbeb1343fb2040cef381d9c47561d796ba11b
phillybebe <philia.hammond@eggseed.com>
2024-01-14 22:20:49 +0000

    fixes

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -9,7 +9,7 @@ function Hero() {
         // backgroundRepeat: "no-repeat",
         // backgroundPosition: "center center"
      }}
-     className="2xl:pb-[32rem] 2xl:pr-[56rem] xl:pb-72 xl:pr-[45rem] lg:pl-40 lg:pr-[38rem] md:pt-32 md:pb-28 md:px-48 lg:bg-[url('../../../resources/assets/home/desktop/image-hero.jpg')] w-full pt-28 pb-24 px-4 bg-[url('../../../resources/assets/home/tablet/image-header.jpg')] bg-cover bg-center bg-no-repeat">
+     className="2xl:pb-[32rem] 2xl:pr-[56rem] xl:pb-72 xl:pr-[45rem] lg:pl-40 lg:pr-[38rem] md:pt-32 md:pb-28 md:px-48 lg:bg-[url('../../../resources/assets/home/desktop/image-hero.jpg')] w-full pt-28 pb-24 px-4 sm:bg-[url('../../../resources/assets/home/tablet/image-header.jpg')] bg-cover bg-center bg-no-repeat">
      <div className="2xl:mt-40 xl:mt-20">
        <p className="lg:text-left md:mb-6 text-[#FFF] text-center text-sm font-normal tracking-[10px] opacity-50 mb-4">NEW PRODUCT</p>
        <h4 className="lg:text-left lg:text-[3.5rem] lg:leading-[58px] lg:tracking-[2px] md:mb-6 text-[#FFF] text-center text-4xl font-bold tracking-[1.286px] leading-[40px] mb-6">XX99 Mark II HeadphoneS</h4>
